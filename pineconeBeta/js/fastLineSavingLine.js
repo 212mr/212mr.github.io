b91b1cb8f15f7574963a2a1da38597a05b7dd3a7
Mengru Zhang <zhang_mengru@126.com>
2026-01-02 22:28:15 -0800

    Remove Google Maps API key to improve security

--- pineconeBeta/js/fastLineSavingLine.js
+++ pineconeBeta/js/fastLineSavingLine.js
@@ -1,12 +1,12 @@
 
 // 谷歌地图
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
-}
+// var map;
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -34.397, lng: 150.644},
+//         zoom: 8
+//     });
+// }
 
 // 运费计算器
 function calculate() {
@@ -15,34 +15,34 @@ function calculate() {
     var fastFinalTotal; //极速最终价格
 
 
-    var packageLength  = parseInt(document.getElementById('packageLength').value);
-    var packageWidth   = parseInt(document.getElementById('packageWidth').value);
-    var packageHeight  = parseInt(document.getElementById('packageHeight').value);
-    var packageWeight  = parseInt(document.getElementById('packageWeight').value);
+    var packageLength = parseInt(document.getElementById('packageLength').value);
+    var packageWidth = parseInt(document.getElementById('packageWidth').value);
+    var packageHeight = parseInt(document.getElementById('packageHeight').value);
+    var packageWeight = parseInt(document.getElementById('packageWeight').value);
     var volume = packageLength * packageWidth * packageHeight;
-    var fastWeight = Math.ceil(packageWeight/100); //向上取整
+    var fastWeight = Math.ceil(packageWeight / 100); //向上取整
     var fastWeightPrice = fastWeight * 1.2;
     var fastVolumePrice = (packageLength * packageWidth * packageHeight) / 5000 * 10 * 1.2; //体积重
-    var fastTotal = Math.max(fastWeightPrice,fastVolumePrice); //取较高价格
+    var fastTotal = Math.max(fastWeightPrice, fastVolumePrice); //取较高价格
 
 
 
-    if (packageLength > 120 || packageWidth > 120 || packageHeight > 120 ){
+    if (packageLength > 120 || packageWidth > 120 || packageHeight > 120) {
         fastFinalTotal = fastTotal * 90 / 100 + 180;
         console.log("超费180")
     } else if (fastTotal >= 24) {
-        fastFinalTotal = fastTotal * 90/100;
+        fastFinalTotal = fastTotal * 90 / 100;
         console.log("九折")
     } else {
         fastFinalTotal = fastTotal;
         console.log(0)
     }
 
-// *****
+    // *****
 
     var savingFinalTotal; //
     var savingWeightPrice;
-    var savingWeight = Math.ceil(packageWeight/1000); //向上取整
+    var savingWeight = Math.ceil(packageWeight / 1000); //向上取整
 
     if (savingWeight < 3) {
         savingWeightPrice = 15;
@@ -53,10 +53,10 @@ function calculate() {
 
 
     var savingVolumePrice = (packageLength * packageWidth * packageHeight) / 6000 * 5; //体积重
-    var savingTotal = Math.max(savingWeightPrice,savingVolumePrice); //取较高价格
+    var savingTotal = Math.max(savingWeightPrice, savingVolumePrice); //取较高价格
     var savingOverSize = packageLength + 2 * (packageWidth + packageHeight);
 
-    if (packageLength > 110){
+    if (packageLength > 110) {
         savingFinalTotal = savingTotal + 20;
         console.log("超110");
     } else if (savingOverSize > 300) {
@@ -66,10 +66,10 @@ function calculate() {
     } else {
         savingFinalTotal = savingTotal;
     }
-// *****
+    // *****
 
-    
-    
+
+
 
     // 显示计算结果 display final price
     document.getElementById('volume').value = volume;
